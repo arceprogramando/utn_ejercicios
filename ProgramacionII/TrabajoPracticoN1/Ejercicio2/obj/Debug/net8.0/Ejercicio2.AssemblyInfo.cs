8ec70a73b4cf737fa0c12157330cea13059f90ee
Felipe Arce <arceprogramando@gmail.com>
2024-08-22 15:17:45 -0300

    feat: exercises 41

--- ProgramacionII/TrabajoPracticoN1/Ejercicio2/obj/Debug/net8.0/Ejercicio2.AssemblyInfo.cs
+++ ProgramacionII/TrabajoPracticoN1/Ejercicio2/obj/Debug/net8.0/Ejercicio2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ce5335226af4f74a28af31ffc276b141155f4fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d151ebe0f4736f85d7741a593b83761c7286fad2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ProgramacionII/TrabajoPracticoN1/Ejercicio41/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+Ejercicio 41
+Escriba un programa que simule un lanzamiento de moneda . Por cada lanzamiento de moneda, el
+programa deberá imprimir Cara o Cruz. Permita que el programa lance la moneda 100 veces y
+cuente el número de veces que aparece cada lado de la moneda. Imprima los resultados. El
+programa debe llamar a una función aparte llamada resultado, la cual no tiene argumentos y
+devuelve 0 para Cara y 1 para Cruz. [Nota: Si el programa simula de manera realista el lanzamiento
+de monedas, entonces cada lado debe aparecer aproximadamente la mitad de las veces, para un
+total de 50 Caras y 50 Cruces.]
+*/
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bienvenido al lanzamiento de moneda");
+
+        int contadorCara = 0;
+        int contadorCruz = 0;
+
+        for (int i = 0; i < 100; i++)
+        {
+            int resultadoactual = Resultado();
+
+            if (resultadoactual == 0)
+            {
+                contadorCara++;
+                Console.WriteLine("Cara");
+
+            }
+            if (resultadoactual == 1) {
+                contadorCruz++;
+                Console.WriteLine("Cruz");
+
+            }
+        }
+
+        Console.WriteLine($"ContadorCara: {contadorCara}");
+        Console.WriteLine($"ContadorCruz: {contadorCruz}");
+
+
+    }
+
+    static int Resultado()
+    {
+        Random random = new();
+        int randomValue = random.Next(2);
+        return randomValue;
+    }
+}
