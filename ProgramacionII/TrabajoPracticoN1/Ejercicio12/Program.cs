2ed323967703e662e91ecc8e3848557f6b64243f
Felipe Arce <arceprogramando@gmail.com>
2024-08-26 18:30:43 -0300

    feat: exercise 12

---
+++ ProgramacionII/TrabajoPracticoN1/Ejercicio12/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Ejercicio 12
+Crear un programa que use el operador condicional para mostrar un el valor absoluto de un número
+de la siguiente forma: si el número es positivo, se mostrará tal cual; si es negativo, se mostrará
+cambiado de signo.
+*/
+
+class Program
+{
+    static void Main()
+    {
+        int numero = LeerNumero("Ingrese un número negativo o positivo: ");
+        Console.WriteLine(numero > 0 ? numero.ToString() : (-numero).ToString());
+    }
+
+    static int LeerNumero(string mensaje)
+    {
+        while (true)
+        {
+            Console.Write(mensaje);
+            string? input = Console.ReadLine();
+
+            if (int.TryParse(input, out int numero))
+            {
+                return numero;
+            }
+
+            Console.WriteLine("Por favor, introduce un número negativo o positivo");
+        }
+    }
+}
