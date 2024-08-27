bfc53bd256d349706d4294d37fdddc78fa7362d6
Felipe Arce <arceprogramando@gmail.com>
2024-08-26 22:46:58 -0300

    feat: exercise 17

---
+++ ProgramacionII/TrabajoPracticoN1/Ejercicio17/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Ejercicio 17
+Diseña un algoritmo que calcule el IVA (16%) de un producto dado su precio de venta sin IVA.
+*/
+
+class Program
+{
+    static void Main()
+    {
+        decimal precio = LeerNumero("Ingrese el precio: ");
+
+        Console.WriteLine($"El precio de el producto es ${precio}");
+        Console.WriteLine($"El precio del producto con IVA es ${precio * 1.16m}");
+
+    }
+
+    static decimal LeerNumero(string mensaje)
+    {
+        while (true)
+        {
+            Console.Write(mensaje);
+            string? input = Console.ReadLine();
+
+            if (decimal.TryParse(input, out decimal numero))
+            {
+                return numero;
+            }
+
+            Console.WriteLine("Por favor, introduce un número:");
+        }
+    }
+}
